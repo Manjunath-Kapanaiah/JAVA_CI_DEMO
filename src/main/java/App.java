712bd6346c43f88420d19fe73a4c834823094a1c
Manjunath H K <183256802+Manjunath-Kapanaiah@users.noreply.github.com>
2026-06-27 14:01:44 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Simple Java App");
-        if (args.length > 0) {
-            System.out.println("Args:");
-            for (String a : args) {
-                System.out.println(" - " + a);
-            }
-        } else {
-            System.out.println("No arguments provided.");
-        }
-
-        public class App {
+public class App {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins CI Demo!");
+        
+        // Display app information
+        System.out.println("Application: Simple Java App");
+        System.out.println("Status: Running successfully");
+        
+        // Process command line arguments if provided
+        if (args.length > 0) {
+            System.out.println("\nArguments provided:");
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("  [" + i + "] " + args[i]);
+            }
+        } else {
+            System.out.println("\nNo arguments provided.");
+        }
+        
+        // Simple calculation example
+        int sum = add(5, 3);
+        System.out.println("\nExample calculation: 5 + 3 = " + sum);
     }
-}
+    
+    // Simple utility method
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
