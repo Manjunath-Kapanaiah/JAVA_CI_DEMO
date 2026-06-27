4d4295120f8c3c0ca1116f553f45ad54617222e0
Manjunath H K <183256802+Manjunath-Kapanaiah@users.noreply.github.com>
2026-06-27 13:11:46 +0530

    app file
    Initial implementation of a simple Java application that prints a message and handles command-line arguments.

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Simple Java App");
+        if (args.length > 0) {
+            System.out.println("Args:");
+            for (String a : args) {
+                System.out.println(" - " + a);
+            }
+        } else {
+            System.out.println("No arguments provided.");
+        }
+    }
+}
